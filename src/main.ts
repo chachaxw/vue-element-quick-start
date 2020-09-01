003ceca8263809c2da28b83a6878d70a153957d8
chacha <867571123@qq.com>
2020-09-01 12:01:10 +0800

    update package.json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
 import App from './App.vue';
 import store from './store/store';
 import router from './router';
